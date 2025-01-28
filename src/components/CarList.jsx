0493be7ea5406869692175cbb699bbcaaaf3051f
emmanuel <emmanjeru2002@gmail.com>
2025-01-28 16:43:51 +0300

    added usestate

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -3,3 +3,10 @@ import "./CarList.css";
 
 const CarList = () => {
   const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/cars")
+      .then((res) => res.json())
+      .then((data) => setCars(data))
+      .catch((err) => console.error("Error fetching cars:", err));
+  }, []);
