6a9a3cb6735d8fc7c2c72602ac22acc94180b88d
emmanuel <emmanjeru2002@gmail.com>
2025-01-28 16:29:37 +0300

    finished my navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,4 +7,18 @@ const Navbar = () => {
         <h1 className="navbar-title">Car Rental</h1>
         <ul className="navbar-links">
           <li>
-         
+            <NavLink to="/cars" activeClassName="active">
+              Cars
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/my-bookings" activeClassName="active">
+              My Bookings
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
