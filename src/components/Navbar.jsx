c5f73180d922494b632827c8d2275ac753a0ad14
emmanuel <emmanjeru2002@gmail.com>
2025-01-29 10:45:53 +0300

    final commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,12 +8,18 @@ const Navbar = () => {
       <h1 className="navbar-title">Car Rental</h1>
       <ul className="navbar-links">
         <li>
-          <NavLink to="/cars" activeClassName="active">
+          <NavLink 
+            to="/cars" 
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
             Cars
           </NavLink>
         </li>
         <li>
-          <NavLink to="/my-bookings" activeClassName="active">
+          <NavLink 
+            to="/my-bookings" 
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
             My Bookings
           </NavLink>
         </li>
