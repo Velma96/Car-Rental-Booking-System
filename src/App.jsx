810da8b4dcd7eabbd32f12a0b2175a7d41369912
phoebe velma awuor <awuorphoebi@gmail.com>
2025-01-29 17:21:10 +0300

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import CarList from './components/CarList';
 import CarDetail from './components/CarDetail';
@@ -10,13 +10,16 @@ const App = () => {
     <div>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Navigate to="/cars" />} /> {/* ✅ Redirect to /cars by default */}
         <Route path="/cars" element={<CarList />} />
         <Route path="/cars/:id" element={<CarDetail />} />
         <Route path="/my-bookings" element={<div>My Bookings</div>} />
+        <Route path="*" element={<div>404 - Page Not Found</div>} /> {/* ✅ Handle invalid routes */}
       </Routes>
     </div>
   );
 };
 
 export default App;
+
 
