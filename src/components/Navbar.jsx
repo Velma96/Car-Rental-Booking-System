84de0ff638ab094925bc2e66dde9739ae87d3e7a
emmanuel <emmanjeru2002@gmail.com>
2025-01-29 04:00:06 +0300

    final commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-title">Car Rental</h1>
+      <ul className="navbar-links">
+        <li>
+          <NavLink to="/cars" activeClassName="active">
+            Cars
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/my-bookings" activeClassName="active">
+            My Bookings
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
